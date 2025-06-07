65877198b43e53f0f66b87fa35cc1e9728694646
MarcHJL <haijinglu.marc@gmail.com>
2025-06-07 12:00:58 +1000

    bind secret, r2, kv

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,49 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import JWT from '@tsndr/cloudflare-worker-jwt';
+import { env } from 'cloudflare:workers';
+
+const allowedOrigins = ['http://localhost:5001', 'http://localhost:3000', 'https://visualchaos.art'];
+
+interface Payload {
+	id: string;
+}
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const origin = request.headers.get('Origin') || '';
+		const corsHeaders = createCorsHeaders(origin, allowedOrigins);
+		const token = request.headers.get('Authorization')?.split(' ')[1];
+
+		if (!token) {
+			return new Response('Unauthorized', { status: 401, headers: corsHeaders });
+		}
+		const payload: Payload | null = await verifyToken(token);
+
+		if (!payload || !payload.id) {
+			return new Response('Unauthorized', { status: 401, headers: corsHeaders });
+		}
+		return new Response('Accessing private image', { headers: corsHeaders });
 	},
 } satisfies ExportedHandler<Env>;
+
+async function verifyToken(token: string) {
+	const decoded = await JWT.verify(token, env.JWT_SECRET!);
+
+	if (!decoded) {
+		return null;
+	}
+	const { payload } = decoded;
+	return payload as Payload;
+}
+
+function createCorsHeaders(origin: string, allowedOrigins: string[]) {
+	const headers: Record<string, string> = {
+		'Access-Control-Allow-Methods': 'GET, PUT, OPTIONS, POST',
+		'Access-Control-Allow-Headers': '*',
+	};
+
+	if (allowedOrigins.includes(origin)) {
+		headers['Access-Control-Allow-Origin'] = origin;
+	}
+
+	return headers;
+}
